07b891367b0baba1772dc24ce76c96579e1e253d
Rafael Bonanno <rafael.bonanno@gmail.com>
2022-11-28 16:56:12 +0100

    added standard methods to trips controller + basic pages controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,51 @@
 class TripsController < ApplicationController
+  def index
+    @trips = Trip.all
+  end
+
+  def show
+    @trip = Trip.find(params[:id])
+    @country = @trip.country
+  end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    @trip.country = @country
+    @trip.user = current_user
+    if @trip.save
+      redirect_to trips_path(@trip)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @trip = Trip.find(params[:id])
+    @country = @trip.country
+  end
+
+  def update
+    @trip = Trip.find(params[:id])
+    @country = @trip.country
+    @trip.user = current_user
+    @trip.update(trip_params)
+    redirect_to trips_path
+  end
+
+  def destroy
+    @trip = Trip.find(params[:id])
+    @country = @trip.country
+    @trip.destroy
+    redirect_to trips_path, status: :see_other
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:start_date, :end_date, :user, :country)
+  end
 end
