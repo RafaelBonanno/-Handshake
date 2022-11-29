4e735152007ae5d730a9b4db7346b039a8745a0e
Rafael Bonanno <rafael.bonanno@gmail.com>
2022-11-29 12:47:21 +0100

    new tweaks on the dashboard

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,7 @@
 class Trip < ApplicationRecord
   belongs_to :user
   belongs_to :country
+  validates :country, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true, comparison: { greater_than: :start_date }
 end
