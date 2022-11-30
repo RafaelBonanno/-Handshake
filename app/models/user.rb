3ad26360e3faa55884d088283ea2520c28271618
Rafael Bonanno <rafael.bonanno@gmail.com>
2022-11-30 13:06:26 +0100

    fixed sign up sign in authenticate

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :email, presence: true
   validates :password, presence: true
+  validates :username, presence: true
   has_many :trips, dependent: :destroy
 end
