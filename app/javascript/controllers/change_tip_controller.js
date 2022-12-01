e868e11dfb1f34ef92277f6a12e8681ae597db22
Rafael Bonanno <rafael.bonanno@gmail.com>
2022-12-01 14:30:43 +0100

    trying my best

--- app/javascript/controllers/change_tip_controller.js
+++ app/javascript/controllers/change_tip_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="change-tip"
 export default class extends Controller {
-  static targets = ["tip"]
+  static targets = ["tip1", "tip2"]
   static values = {
     tips: Array
   }
@@ -12,18 +12,32 @@ export default class extends Controller {
   }
 
   changeTip() {
-    console.log("im clicking the button")
-    console.log(this.#changeTipsFromArray)
-    this.tipTarget.classList.add('animate__animated', 'animate__bounceOutLeft');
-    // then(this.#changeTipsFromArray)
-    // this.#changeTipsFromArray();
+    console.log("im clicking the button");
+    this.tip1Target.classList.toggle('animate__animated', 'animate__flash')
+    this.tip1.tip1Target.classList.toggle('display');
+    this.tip1Target.innerText = tipContent1;
+    // this.tipTarget.classList.toggle('animate__animated', 'animate__bounceInRight');
+    const tipContent1 = this.#sampleTipsFromArray();
+
+    // setTimeout(() => {
+    this.tip2Target.classList.toggle('animate__animated', 'animate__flash')
+    this.tip2Target.innerText = tipContent2;
+    const tipContent2 = this.#sampleTipsFromArray();
+
+    // }, 1000)
+    // this.tipTarget.innerText = tipContent;
     // this.tipTarget.classList.add('animate__animated', 'animate__bounceInRight')
     // .then(this.tipTarget.innerText = this.tips.sample)
     // console.log(this.tipTarget.innerText)
     // this.tipTarget.classList.add('animate__bounceInRight')
   }
-  #changeTipsFromArray(){
-    var tip = this[Math.floor(Math.random()*this.length)];
-    console.log(tip)
+  #sampleTipsFromArray(){
+    var tip = this.tipsValue[Math.floor(Math.random()*this.tipsValue.length)];
+    if (tip != tip) {
+      return tip
+    } else {
+      var tip = this.tipsValue[Math.floor(Math.random()*this.tipsValue.length)];
+      return tip
+    }
   }
   }
