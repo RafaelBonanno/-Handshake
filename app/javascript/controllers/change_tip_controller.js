6e76c193255d4ac3c9430d9e26b75f491c890329
Rafael Bonanno <rafael.bonanno@gmail.com>
2022-12-01 10:57:35 +0100

    wip on the js tips

--- app/javascript/controllers/change_tip_controller.js
+++ app/javascript/controllers/change_tip_controller.js
@@ -3,21 +3,16 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="change-tip"
 export default class extends Controller {
   static targets = ["tip"]
-  static values = {
-    tips: Array
-  }
 
   connect() {
     console.log(this.tipsValue)
   }
+
+  changeTip() {
+    console.log("im clicking the button")
+    this.tipTarget.classList.add('animate__animated', 'animate__bounceOutLeft')
+    // .then(this.tipTarget.innerText = this.tips.sample)
+    // console.log(this.tipTarget.innerText)
+    // this.tipTarget.classList.add('animate__bounceInRight')
+  }
 }
-//   changeTip(){
-//     console.log("im clicking the button")
-//     console.log(this.tipTarget.innerText)
-//     console.log(this.tips)
-//     this.tipTarget.classList.add('animate__animated', 'animate__bounceOutLeft')
-//     .then(this.tipTarget.innerText = this.tips.sample)
-//     console.log(this.tipTarget.innerText)
-//     this.tipTarget.classList.add('animate__bounceInRight')
-//   }
-// }
