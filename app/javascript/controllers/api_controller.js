81c4e1db5e8b73158788dc4e04ad5541f75b999e
Rafael Bonanno <rafael.bonanno@gmail.com>
2022-12-05 17:48:11 +0100

    dashboard change frontend

--- app/javascript/controllers/api_controller.js
+++ app/javascript/controllers/api_controller.js
@@ -9,9 +9,7 @@ export default class extends Controller {
   }
 
   fetchSpeech(event) {
-    console.log("I just clicked to ask the API")
-    console.log(this.soundTarget.textContent)
-    let query = this.soundTarget.textContent
+    let query = this.soundTarget.outerText
     let code = event.currentTarget.dataset.lang
     fetch(`http://api.voicerss.org/?key=475e01e01a48448189640b5842ece531&hl=${code}&src=${query}`)
       .then(response => new Audio(response.url).play())
