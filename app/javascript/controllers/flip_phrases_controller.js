22dd15210982c402ca004ca8a900e7a21b2b5194
Rafael Bonanno <rafael.bonanno@gmail.com>
2022-12-07 15:31:00 +0100

    fixed flipcards

--- app/javascript/controllers/flip_phrases_controller.js
+++ app/javascript/controllers/flip_phrases_controller.js
@@ -7,10 +7,22 @@ export default class extends Controller {
     console.log("hello from flipcard controller")
   }
 
-  flipPhrase() {
-    this.flipcardTarget.classList.toggle('is-flipped')
+  flipPhrase(event) {
+    if (!event.target.classList.contains("fa-heart") && !event.target.classList.contains("fa-volume-high")) {
+      this.flipcardTarget.classList.toggle('is-flipped')
+    }
   }
 
+  like(event) {
+    if (event.currentTarget.classList.contains('fa-regular')) {
+    event.currentTarget.classList.replace('fa-regular', 'fa-solid')
+  } else {
+    event.currentTarget.classList.replace('fa-solid', 'fa-regular')
+  }
+
+  }
+  // <i class="fa-regular fa-heart heart-flipcard"></i>
+  // <i class="fa-solid fa-heart heart-flipcard"></i>
 }
 
 // var cards = document.querySelectorAll('.card');
